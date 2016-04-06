843b1030f444dbc2d449b9869dd978c7bf9263c4
Brian Bosak <brielcccc@gmail.com>
2016-04-06 11:23:59 -0500

    About to give up on java interop. Java really sucks.

--- PiOverThirty/MainActivity.cs
+++ PiOverThirty/MainActivity.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿
+
+using System;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-
+using Urho.Droid;
+using Android.Accounts;
 namespace PiOverThirty
 {
     [Activity(Label = "PiOverThirty", MainLauncher = true, Icon = "@drawable/icon")]
@@ -16,7 +19,8 @@ namespace PiOverThirty
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            Com.Facebook.FacebookSdk.SdkInitialize(this);
+            Com.Facebook.Login.LoginManager mngr;
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
@@ -25,6 +29,12 @@ namespace PiOverThirty
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+
+            AbsoluteLayout layout = FindViewById<AbsoluteLayout>(Resource.Id.absoluteLayout1);
+            var surface = UrhoSurface.CreateSurface<AppMain>(this);
+            layout.AddView(surface);
+            
         }
     }
 }
--- PiOverThirty/Resources/Resource.Designer.cs
+++ PiOverThirty/Resources/Resource.Designer.cs
@@ -63,6 +63,9 @@ namespace PiOverThirty
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int absoluteLayout1 = 2131034113;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -92,11 +95,14 @@ namespace PiOverThirty
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040002
+			public const int ApplicationName = 2130968578;
+			
 			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			public const int Hello = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
